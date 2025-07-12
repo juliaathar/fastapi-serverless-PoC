70b813e0d48a4c341393672b7fc14209947c0d56
juliaathar <juathar@gmail.com>
2025-07-12 13:31:01 -0300

    add: adding serverless config

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from .routers import user
 from .database import Base, engine
+from mangum import Mangum
 
 Base.metadata.create_all(bind=engine)
 
@@ -8,6 +9,8 @@ app = FastAPI(
     title='PoC FastAPI com AWS',
     description='Teste de projeto de API python com FastAPI fazendo deploy pra AWS'
 )
+
+handler = Mangum(app)
 
 app.include_router(user.router)
 
